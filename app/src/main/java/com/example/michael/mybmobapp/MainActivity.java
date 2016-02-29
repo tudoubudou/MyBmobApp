cc7b4511e2c5df9f1414ab7053a7a8f2dfaa4729
jieke.guo <ziwen.gzw@alibaba-inc.com>
2016-02-29 20:39:35 +0800

    add taobao sdk

--- app/src/main/java/com/example/michael/mybmobapp/MainActivity.java
+++ app/src/main/java/com/example/michael/mybmobapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.michael.mybmobapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.sdk.android.AlibabaSDK;
+import com.alibaba.sdk.android.ResultCode;
+import com.alibaba.sdk.android.callback.CallbackContext;
+import com.alibaba.sdk.android.callback.InitResultCallback;
+import com.alibaba.sdk.android.login.LoginService;
+import com.alibaba.sdk.android.login.callback.LoginCallback;
+import com.alibaba.sdk.android.login.callback.LogoutCallback;
+import com.alibaba.sdk.android.session.model.Session;
+import com.alibaba.sdk.android.trade.CartService;
+import com.alibaba.sdk.android.trade.ItemService;
+import com.alibaba.sdk.android.trade.TradeConfigs;
+import com.alibaba.sdk.android.trade.TradeService;
+import com.alibaba.sdk.android.trade.callback.TradeProcessCallback;
+import com.alibaba.sdk.android.trade.model.TaokeParams;
+import com.alibaba.sdk.android.trade.model.TradeResult;
+import com.alibaba.sdk.android.trade.page.ItemDetailPage;
+import com.alibaba.sdk.android.trade.page.MyCartsPage;
+import com.alibaba.sdk.android.trade.page.MyOrdersPage;
 import com.example.michael.mybmobapp.model.Person;
 
 import org.json.JSONArray;
@@ -55,11 +74,29 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+        AlibabaSDK.asyncInit(this, new InitResultCallback() {
 
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "初始化成功", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onFailure(int code, String message) {
+                Toast.makeText(MainActivity.this, "初始化异常", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+        });
         Bmob.initialize(this, "14fc2142c3fa7d48d597f565a3540aaf");
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        CallbackContext.onActivityResult(requestCode, resultCode, data);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -74,15 +111,32 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            queryData();
-            return true;
+        switch (id ){
+            case R.id.action_list:
+//                showMyOrdersPage(0,false);
+//                showMyCartsPage();
+//                showItemDetailPage();
+                showPayPage();
+                return true;
+            case R.id.action_settings:
+                queryData();
+                return true;
+            case R.id.action_taobao:
+                showLogin();
+                return true;
+            case R.id.action_get_taobaoke:
+//                showLogin();
+                showItemDetailPage();
+                return true;
+            default:break;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void getTaobaoke() {
+
+    }
     public void queryData(){
         BmobQuery query = new BmobQuery("Person");
         query.findObjects(this, new FindCallback() {
@@ -106,6 +160,162 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void showLogin() {
+        LoginService loginService = AlibabaSDK.getService(LoginService.class);
+        loginService.showLogin(MainActivity.this, new LoginCallback() {
+
+            @Override
+            public void onSuccess(Session session) {
+                Toast.makeText(MainActivity.this, "欢迎" + session.getUser().nick + session.getUser().avatarUrl,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int code, String message) {
+                Toast.makeText(MainActivity.this, "授权取消" + code + message,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void logout(View view) {
+        LoginService loginService = AlibabaSDK.getService(LoginService.class);
+        loginService.logout(this, new LogoutCallback() {
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Toast.makeText(MainActivity.this, "登出失败", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "登出成功", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+    }
+    public void showItemDetailPage(){
+        TradeService tradeService = AlibabaSDK.getService(TradeService.class);
+//        ItemDetailPage itemDetailPage = new ItemDetailPage("AAEdNWF_ABrk9HpuLchgVlYX", null);
+        ItemDetailPage itemDetailPage = new ItemDetailPage("37196464781", null);
+        TaokeParams taokeParams = new TaokeParams(); //若非淘客taokeParams设置为null即可
+        taokeParams.pid = "mm_24942362_0_0";
+        tradeService.show(itemDetailPage, taokeParams, MainActivity.this, null, new TradeProcessCallback() {
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Toast.makeText(MainActivity.this, "失败 " + code + msg,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPaySuccess(TradeResult tradeResult) {
+                Toast.makeText(MainActivity.this, "成功", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+    }
+    public void showMyCartsPage(){
+        TradeService tradeService = AlibabaSDK.getService(TradeService.class);
+        MyCartsPage myCartsPage = new MyCartsPage();
+        TradeConfigs.defaultISVCode = "kdkdkdkd"; //传入isv_code
+        tradeService.show(myCartsPage, null, MainActivity.this, null, new TradeProcessCallback(){
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Toast.makeText(MainActivity.this, "失败 "+code+msg,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPaySuccess(TradeResult tradeResult) {
+                Toast.makeText(MainActivity.this, "成功", Toast.LENGTH_SHORT)
+                        .show();
+
+            }});
+    }
+    public void addItem2Cart(String openId) {
+        CartService cartService = AlibabaSDK.getService(CartService.class);
+        cartService.addItem2Cart(this, new TradeProcessCallback() {
+
+            @Override
+            public void onPaySuccess(TradeResult tradeResult) {
+                Toast.makeText(MainActivity.this, "支付成功", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                if (code == ResultCode.QUERY_ORDER_RESULT_EXCEPTION.code) {
+                    Toast.makeText(MainActivity.this, "确认交易订单失败",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "交易取消 " + code + msg,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, "加入购物车", openId, null);
+    }
+    /**
+     * 我的订单页
+     *
+     * @param status
+     *            默认跳转页面；填写：0：全部；1：待付款；2：待发货；3：待收货；4：待评价。若传入的不是这几个数字，则跳转到“全部”页面且“allOrder”失效
+     * @param allOrder true：显示全部订单。False：显示当前appKey下的订单
+     *
+     */
+    public void showMyOrdersPage(int status, boolean allOrder){//to order management page
+        TradeService tradeService = AlibabaSDK.getService(TradeService.class);
+        MyOrdersPage myOrdersPage = new MyOrdersPage(status, allOrder);
+        tradeService.show(myOrdersPage, null, MainActivity.this, null, new TradeProcessCallback() {
+
+            @Override
+            public void onFailure(int code, String msg) {
+                Toast.makeText(MainActivity.this, "失败 " + code + msg,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPaySuccess(TradeResult tradeResult) {
+                Toast.makeText(MainActivity.this, "成功", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+    }
+    public void showPayPage() {
+        ItemService itemService = AlibabaSDK.getService(ItemService.class);
+        itemService.showPage(this, new TradeProcessCallback() {
+
+            @Override
+            public void onPaySuccess(TradeResult tradeResult) {
+                Toast.makeText(
+                        MainActivity.this,
+                        "支付成功" + tradeResult.paySuccessOrders + "   "
+                                + tradeResult.payFailedOrders,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFailure(int code, String msg) {
+                if (code == ResultCode.QUERY_ORDER_RESULT_EXCEPTION.code) {
+                    Toast.makeText(MainActivity.this, "确认交易订单失败",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "交易异常",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, null, "http://m.taobao.com");
+    }
     private void showToast(String s) {
         Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
     }
